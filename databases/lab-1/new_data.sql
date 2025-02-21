44aa25ea7b72b73439b79c0cc84b08092a3ab9a3
ArtemijKarandashov <158590081+ArtemijKarandashov@users.noreply.github.com>
2025-02-21 13:37:50 +0300

    Add files via upload

---
+++ databases/lab-1/new_data.sql
@@ -0,0 +1,5 @@
+INSERT INTO `simpledb`.`users` (`name`, `email`, `postal_code`, `gender`, `bday`,
+`rating`) VALUES ('Ekaterina', 'ekaterina.petrova@outlook.com', '145789', 'f',
+'2000-02-11', '1.123');
+INSERT INTO `simpledb`.`users` (`name`, `email`, `postal_code`, `gender`, `bday`,
+`rating`) VALUES ('Paul', 'paul@superpochta.ru', '123789', 'm', '1998-08-12', '1');
