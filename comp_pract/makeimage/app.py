0f073bb6af1dde758c8de95ad14e9310cac5782b
ArtemijKarandashov <158590081+ArtemijKarandashov@users.noreply.github.com>
2025-04-24 11:52:00 +0300

    Update app.py

--- comp_pract/makeimage/app.py
+++ comp_pract/makeimage/app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, request, Response
 from flask_cors import CORS
 from PIL import Image
 
-import numpy as np
 import io
 
 app = Flask(__name__)
@@ -15,13 +14,25 @@ def handle():
 
 @app.route('/login')
 def login():
-    return '1149920'
+    return {'author':'1149920'}
 
 
-@app.route('/makeimage', methods=['GET'])
+@app.route('/makeimage', methods=['GET','POST'])
 def handle_makeimage():
-    width = int(request.args.get('width'))
-    height = int(request.args.get('height'))
+    width = 0
+    height = 0
+
+    if request.method == 'GET':
+        width = int(request.args.get('width'))
+        height = int(request.args.get('height'))
+    
+    if request.method == 'POST':
+        data = request.json
+        width = int(data('width'))
+        height = int(data('height'))
+    
+    if width < 1 or height < 1:
+        return {'message':'Invalid image size'}
     
     img = Image.new(mode='RGB',size=(width,height), color='green')
 
